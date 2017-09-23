d1f56a54f66fe04a11b3fb5b0ca19b55a2a75dbe
Xiong <xiokenw@dunwoody.edu>
2017-09-22 19:15:22 -0500

    commiting all files for lab1

---
+++ hipHopArtists.js
@@ -0,0 +1,146 @@
+var hipHopArtists = [
+    {
+      "name": "Wu-Tang Clan",
+      "city": "Staten Island, NY",
+      "albums": 6,
+      "copiesSold": "40 million"
+    },
+    {
+      "name": "Eminem",
+      "city": "Detroit, MI",
+      "albums": 8,
+      "copiesSold": "172 million"
+    },
+    {
+      "name": "Ice Cube",
+      "city": "Los Angeles, CA",
+      "albums": 10,
+      "copiesSold": "25 million"
+    },
+    {
+      "name": "Warren G",
+      "city": "Long Beach, CA",
+      "albums": 6,
+      "copiesSold": "4 million"
+    },
+    {
+      "name": "Outkast",
+      "city": "Atlanta, GA",
+      "albums": 6,
+      "copiesSold": "25 million"
+    },
+    {
+      "name": "UGK",
+      "city": "Port Arthur, TX",
+      "albums": 6,
+      "copiesSold": "3.5 million"
+    },
+    {
+      "name": "Nas",
+      "city": "Queens, NY",
+      "albums": 10,
+      "copiesSold": "15 million"
+    },
+    {
+      "name": "Snoop Dogg",
+      "city": "Long Beach, CA",
+      "albums": 15,
+      "copiesSold": "37 million"
+    },
+    {
+      "name": "Cypress Hill",
+      "city": "South Gate, CA",
+      "albums": 8,
+      "copiesSold": "5 million"
+    },
+    {
+      "name": "Tech N9ne",
+      "city": "Kansas City, MO",
+      "albums": 18,
+      "copiesSold": "2 million"
+    },
+    {
+      "name": "2pac",
+      "city": "Los Angeles, CA",
+      "albums": 5,
+      "copiesSold": "42.5 million"
+    },
+    {
+      "name": "Notorious B.I.G.",
+      "city": "Brooklyn, NY",
+      "albums": 5,
+      "copiesSold": "17 million"
+    },
+    {
+      "name": "Mobb Deep",
+      "city": "Queens, NY",
+      "albums": 8,
+      "copiesSold": "2 million"
+    },
+    {
+      "name": "T.I.",
+      "city": "Atlanta, GA",
+      "albums": 9,
+      "copiesSold": "?"
+    },
+    {
+      "name": "Kendrick Lamar",
+      "city": "Compton, CA",
+      "albums": 4,
+      "copiesSold": "5 million"
+    },
+    {
+      "name": "Childish Gambino",
+      "city": "Edwards Airforce Base, CA",
+      "albums": 3,
+      "copiesSold": "1.5 million"
+    },
+    {
+      "name": "J. Cole",
+      "city": "Raleigh, NC",
+      "albums": 4,
+      "copiesSold": "3 million"
+    },
+    {
+      "name": "Big K.R.I.T",
+      "city": "Meridian, MS",
+      "albums": 2,
+      "copiesSold": "300,000"
+    },
+    {
+      "name": "Kanye West",
+      "city": "Chicago, IL",
+      "albums": 7,
+      "copiesSold": "66 million"
+    },
+    {
+      "name": "DMX",
+      "city": "Yonkers, NY",
+      "albums": 7,
+      "copiesSold": "74 million"
+    },
+    {
+      "name": "Yelawolf",
+      "city": "Gadsen, AL",
+      "albums": 3,
+      "copiesSold": "300,000"
+    },
+    {
+      "name": "KRS-One",
+      "city": "South Bronx, NY",
+      "albums": 13,
+      "copiesSold": "?"
+    },
+    {
+      "name": "Big L",
+      "city": "Harlem, NY",
+      "albums": 2,
+      "copiesSold": "270,000"
+    },
+    {
+      "name": "Atmosphere",
+      "city": "Minneapolis, MN",
+      "albums": 8,
+      "copiesSold": "?"
+    }
+  ]
